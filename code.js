41afd3f1df5c53631c5b108b3f81318c1856c71d
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-09-04 15:48:07 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
 function binarySearch(list, element) {
-    var midpoint = (list.length() / 2) - 1;
+    var midpoint = (list.length / 2) - 1;
     var mid = list[midpoint];
     if (element > mid) {
         //low = mid;
-        list = list.slice(list[midpoint+1], list.length());
+        list = list.slice(list[midpoint+1], list.length);
         binarySearch(list, element);
     if (element < mid) {
         list = list.slice(0, list[midpoint]);
