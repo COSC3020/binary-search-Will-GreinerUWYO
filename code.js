d1026d246b03aa10c9548bc1a5fd0bdc2214ec4f
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-09-24 09:36:20 -0600

    Update code.js

--- code.js
+++ code.js
@@ -2,19 +2,17 @@ function binarySearch(list, element) {
     var low = 0;
     var high = list.length - 1;
     var mid = high/2 - 1;
-    var temp;
-
-    while (low <= high) {
+    while (low < high) {
         //Found the target!
         if (list[mid] == element) {
             return mid;
         }
         //Undershot the target!
-        if (list[mid] > element) {
+        else if (list[mid] > element) {
             low = mid + 1;
         }
         //Overshot the target!
-        if (list[mid] < element) {
+        else if (list[mid] < element) {
             high = mid - 1;
         }
     }
